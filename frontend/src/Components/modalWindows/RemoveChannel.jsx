cea5750233ced961055741b2abc36e38c918203b
marina rodina <mrodina1990@yandex.ru>
2024-01-11 01:11:21 +0500

    add channel deletion

---
+++ frontend/src/Components/modalWindows/RemoveChannel.jsx
@@ -0,0 +1,72 @@
+import React, { useRef } from 'react';
+import { Form, Button } from 'react-bootstrap';
+import Modal from 'react-bootstrap/Modal';
+import FormGroup from 'react-bootstrap/FormGroup';
+import { useFormik } from 'formik';
+import { useSelector, useDispatch } from 'react-redux';
+import useSocket from '../../Hooks/useSocket.jsx';
+import { actions as modalsActions } from '../../Slices/modalsSlice.js';
+
+const RemoveChannel = () => {
+    const socketChat = useSocket();
+    const dispatch = useDispatch();
+    const onHide = () => dispatch(modalsActions.closeModal());
+    const modalInfo = useSelector((state) => state.modalsReducer.setModalInfo);
+    const channelId = modalInfo.targetId;
+
+    const {
+        handleSubmit, setSubmitting, isSubmitting,
+    } = useFormik({
+        initialValues: {
+            removingChannelId: null,
+        },
+        onSubmit: () => {
+            setSubmitting(true);
+            socketChat.removeChannel(channelId)
+                .then(() => {
+                    onHide();
+                })
+                .catch((error) => {
+                    console.log('ERROR', error);
+                })
+                .finally(() => {
+                    setSubmitting(false);
+                });
+        },
+    });
+
+    const inputRef = useRef(null);
+
+    return (
+        <Modal show centered>
+            <Modal.Header closeButton onClick={onHide}>
+                <Modal.Title className="modal-title h4">Удалить канал</Modal.Title>
+            </Modal.Header>
+            <Form onSubmit={handleSubmit}>
+                <Modal.Body>
+                    <p className="lead">Уверены?</p>
+                </Modal.Body>
+                <FormGroup className="d-flex justify-content-end m-3">
+                    <Button
+                        className="me-2 btn-secondary"
+                        variant="secondary"
+                        onClick={() => onHide()}
+                    >
+                        Отменить
+                    </Button>
+                    <Button
+                        className="btn-primary"
+                        type="submit"
+                        variant="danger"
+                        ref={inputRef}
+                        disabled={isSubmitting}
+                    >
+                        Удалить
+                    </Button>
+                </FormGroup>
+            </Form>
+        </Modal>
+    );
+};
+
+export default RemoveChannel;
--- frontend/src/Components/modalWindows/index.js
+++ frontend/src/Components/modalWindows/index.js
@@ -1,7 +1,9 @@
 import AddChannelModal from './AddNewChannel.jsx';
+import RemoveChannel from './RemoveChannel.jsx';
 
 const modals = {
   add: AddChannelModal,
+  delete: RemoveChannel,
 };
 
 export default (channelName) => modals[channelName];
