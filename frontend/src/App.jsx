18a7ba6b54fdbdae4cb130bbaab0649ae8b076a1
marina rodina <mrodina1990@yandex.ru>
2024-01-17 10:15:32 +0500

    add NotFoundPage.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ import ChatPage from './Components/ChatPage.jsx';
 import LoginPage from './Components/LoginPage.jsx';
 import RegistrationPage from './Components/RegistrationPage.jsx';
 import HeaderComponent from './Components/HeaderComponent.jsx';
-import NotFound from './Components/NotFound.jsx';
+import NotFoundPage from './Components/NotFoundPage.jsx';
 import AuthProvider from './Components/AuthProvider.jsx';
 import routes from './Routes.js';
 import useAuth from './Hooks/useAuth.jsx';
@@ -25,10 +25,10 @@ const App = () => (
         <div className="d-flex flex-column h-100">
           <HeaderComponent />
           <Routes>
-            <Route path="*" element={<NotFound />} />
             <Route path={routes.chatPagePath()} element={<RoutePrivate><ChatPage /></RoutePrivate>} />
             <Route path={routes.loginPagePath()} element={<LoginPage />} />
             <Route path={routes.signupPagePath()} element={<RegistrationPage />} />
+            <Route path="*" element={<NotFoundPage />} />
           </Routes>
           <ModalComponent />
         </div>
