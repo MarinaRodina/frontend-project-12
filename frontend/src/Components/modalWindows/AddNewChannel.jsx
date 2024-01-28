44e8fa61f85186c7883c9e83c206bfb5f27cc1d4
marina rodina <mrodina1990@yandex.ru>
2024-01-29 00:24:41 +0500

    fix message for user

--- frontend/src/Components/modalWindows/AddNewChannel.jsx
+++ frontend/src/Components/modalWindows/AddNewChannel.jsx
@@ -85,7 +85,7 @@ const AddNewChannel = () => {
               onChange={handleChange}
             />
             <Form.Label className="visually-hidden" htmlFor="channelName">{t('modals.channelName')}</Form.Label>
-            <p>{t('modals.text')}</p>
+            <div>{t('modals.text')}</div>
             <div className="invalid-feedback">{errors.channelName}</div>
           </Modal.Footer>
         </Form.Group>
